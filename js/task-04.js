bddd1d3afe194acae5b8feea818d5b75a61b533b
AntonBereza1995 <80589071+AntonBereza1995@users.noreply.github.com>
2021-08-06 10:00:38 +0300

    правки

--- js/task-04.js
+++ js/task-04.js
@@ -1,9 +1,3 @@
-// Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
-
-// Создай переменную counterValue в которой будет хранится текущее значение счетчика.
-// Создай функции increment и decrement для увеличения и уменьшения значения счетчика
-// Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса.
-
 const buttonDecrement = document.querySelector('button[data-action="decrement"]');
 const buttonIncrement = document.querySelector('button[data-action="increment"]');
 const counterSpan = document.querySelector("#value");
